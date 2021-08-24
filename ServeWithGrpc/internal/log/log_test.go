d0783b78f4122e6258f3af6e57b7b277605642c9
Idiakose O. Sunday <idiakosesunday@gmail.com>
2021-08-24 22:35:08 +0100

    Use custom error with details

--- ServeWithGrpc/internal/log/log_test.go
+++ ServeWithGrpc/internal/log/log_test.go
@@ -49,7 +49,9 @@ func testAppendRead(t *testing.T, log *Log) {
 func testOutOfRange(t *testing.T, log *Log) {
 	read, err := log.Read(1)
 	require.Nil(t, read)
+	apiErr := err.(api.ErrOffsetOutOfRange)
 	require.Error(t, err)
+	require.Equal(t, uint64(1), apiErr.Offset)
 }
 
 func testInitExisting(t *testing.T, log *Log) {
